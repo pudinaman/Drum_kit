e106749de12f56cf7c2c25b58f7c5cb4c52c48b0
ayush <ayush.raj2019@outlook.com>
2023-11-02 23:10:28 +0530

    cosole_messges

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var n=document.querySelectorAll(".drum").length;
 for(var i=0;i<n;i++){
 document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-    alert("i got clicked");
+  console.log(this);
 });
 }
+
+
+
+
+//var audio= new Audio('sounds/tom-1.mp3');
+  // audio.play();
