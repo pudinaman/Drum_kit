26c1fd797f956c0e4c90aee0eb55433555463ea1
ayush <ayush.raj2019@outlook.com>
2023-11-05 11:21:43 +0530

    added_keybord_functionaliites)

--- index.js
+++ index.js
@@ -3,38 +3,46 @@ var n = document.querySelectorAll(".drum").length;
 for (var i = 0; i < n; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHtml = this.innerHTML;
-    switch (buttonInnerHtml) {
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case "s":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-      case "j":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-      case "k":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-      case "l":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-      default:
-        console.log("Invalid key");
-        break;
-    }
+    makeSound(buttonInnerHtml);
+    
   });
+}
+document.addEventListener("keydown",function(event){
+   // alert("key was pressed");
+  makeSound(event.key)
+})
+function makeSound(key){
+    switch (key) {
+        case "w":
+          var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+        case "a":
+          var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+        case "s":
+          var tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+        case "d":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+          break;
+        case "j":
+          var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+        case "k":
+          var crash = new Audio("sounds/crash.mp3");
+          crash.play();
+          break;
+        case "l":
+          var kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+          break;
+        default:
+          console.log("Invalid key");
+          break;
+      }
 }
